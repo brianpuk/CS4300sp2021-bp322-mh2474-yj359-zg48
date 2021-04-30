12d773e461cd2eba7eb224a32de83fc186b2f6f6
Brian Pukmel <31942897+brianpuk@users.noreply.github.com>
2021-04-30 14:33:47 -0400

    Update __init__.py

--- app/irsystem/models/__init__.py
+++ app/irsystem/models/__init__.py
@@ -4,6 +4,10 @@ from werkzeug import check_password_hash, generate_password_hash # Hashing
 import hashlib # For session_token generation (session-based auth. flow)
 import datetime # For handling dates 
 
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+
+
 class Base(db.Model):
   """Base PostgreSQL model"""
   __abstract__ = True
